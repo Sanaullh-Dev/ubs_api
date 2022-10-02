6a5c4d931459a1255c17249defad28cbdd984724
Sanaulla <sanuallashaikh1991@gmail.com>
2022-10-02 22:28:46 +0530

    AWS database used

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,15 +2,24 @@
 //     HOST : "localhost",
 //     USER : "root",
 //     PASSWORD : "Sanaulla0405",
-//     // PASSWORD : "Shakti@123",
+//     PASSWORD : "Shakti@123",
 //     DB : "ubs"
 // }
 
-// online database 
+// online database free sql database
+// module.exports = {
+//     host : "sql6.freesqldatabase.com",
+//     user : "sql6522714",
+//     password : "hNY39EPh6g",
+//     database : "sql6522714",
+//     port : 3306
+// }
+
+// AWS mysql database 
 module.exports = {
-    host : "sql6.freesqldatabase.com",
-    user : "sql6522714",
-    password : "hNY39EPh6g",
-    database : "sql6522714",
+    host : "ubs-database.cx9ieclkbphh.us-east-1.rds.amazonaws.com",
+    user : "admin",
+    password : "Sanaulla#0405",
+    database : "ubs",
     port : 3306
 }
